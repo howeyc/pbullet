33e3d192ad312a2d914de0fd70194555b936a18a
Chris Howey <chris@howey.me>
2013-03-23 12:57:00 -0500

    Add other push types.

--- main.go
+++ main.go
@@ -32,6 +32,26 @@ func (pd *PushDev) PushNote(title, body string) (resp *http.Response, err error)
 	return http.PostForm(pushUrl, pushVals)
 }
 
+func (pd *PushDev) PushAddress(name, address string) (resp *http.Response, err error) {
+	pushVals := url.Values{}
+	pushVals.Set("device_id", pd.Id)
+	pushVals.Set("type", "note")
+	pushVals.Set("name", name)
+	pushVals.Set("address", address)
+
+	return http.PostForm(pushUrl, pushVals)
+}
+
+func (pd *PushDev) PushLink(title, urlAddress string) (resp *http.Response, err error) {
+	pushVals := url.Values{}
+	pushVals.Set("device_id", pd.Id)
+	pushVals.Set("type", "note")
+	pushVals.Set("title", title)
+	pushVals.Set("url", urlAddress)
+
+	return http.PostForm(pushUrl, pushVals)
+}
+
 func main() {
 	SetAPIKey("e4ac3e11929d522888c58ed67268b643")
 
